dfd52e2981cded5881f5270be8d8f05cba01ce3b
Luis <deathghoul41@gmail.com>
2023-08-22 00:09:44 +0200

    final

--- src/components/Auth/Signup/RegistrationForm.jsx
+++ src/components/Auth/Signup/RegistrationForm.jsx
@@ -8,8 +8,14 @@ import Google from '../../../assets/Login/google.png'
 
 import GoogleAuth from '../GoogleAuth';
 const RegistrationForm = () => {
-  /* ------------------------Handle Registration ----------------------*/
   const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    password1: '',
+    password2: '',
+  });
+
+  const [errors, setErrors] = useState({
     username: '',
     email: '',
     password1: '',
@@ -19,6 +25,7 @@ const RegistrationForm = () => {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    setErrors({ ...errors, [name]: '' }); // Clear previous errors
   };
 
   const handleSubmit = async (e) => {
@@ -29,56 +36,56 @@ const RegistrationForm = () => {
       window.location.reload()
     } catch (error) {
       console.error('Registration failed', error);
+
+      // Update errors state with validation errors from the server
+      if (error.response && error.response.data) {
+        setErrors(error.response.data);
+      }
     }
   };
-/* ------------------------Handle Registration ----------------------*/
-const [showLogin,setShowLogin] = useState(false);
 
-const handleShowLogin = () => {
-  setShowLogin(!showLogin);
-}
-/* ------------------------Toggle Login ----------------------*/
+  const [showLogin, setShowLogin] = useState(false);
 
-  return (  
+  const handleShowLogin = () => {
+    setShowLogin(!showLogin);
+  }
+
+  return (
     <div className='app-login-group'>
       {showLogin ? (<Login/>) : (
-      <div className='app-login'>
-        <div className='app-registration-leftside'>
-          <div className='app-registration-title-group'>
-          <p className='app-registration-title'>Sign up to DoneMinder</p>
-          <button className='app-registration-google'><img src={Google}/> Sign up with Google</button>
+        <div className='app-login'>
+          <div className='app-registration-leftside'>
+            <div className='app-registration-title-group'>
+              <p className='app-registration-title'>Sign up to DoneMinder</p>
+              <button className='app-registration-google'><img src={Google}/> Sign up with Google</button>
+            </div>
+            <p className='app-registration-already'>Already Registered?
+              <button className='app-registration-already-button' onClick={handleShowLogin}>Sign in</button>
+            </p>
           </div>
-          <p className='app-registration-already'>Already Registered?
-          <button className='app-registration-already-button' onClick={handleShowLogin}>Sign in</button></p>
-        </div>
-        <div className='app-registration-middle-line'>
+          <div className='app-registration-middle-line'>
             <div className='line-1'/>
-              <p className='middle-text-or'>Or</p>
+            <p className='middle-text-or'>Or</p>
             <div className='line-1'/>
+          </div>
+          <form className='form-group-max' onSubmit={handleSubmit}>
+            <div className='form-input-container-username'>
+              <div className='form-input-container-group'>
+                <label className='form-label-text'>Username</label>
+                <input
+                  className='input-container-username'
+                  type="text"
+                  name="username"
+                  onChange={handleInputChange}
+                />
+                <span className='error-message'>{errors.username}</span>
+              </div>
+              {/* Other input fields */}
+              <button className='form-input-submit' type="submit">Create Account</button>
+            </div>
+          </form>
         </div>
-    <form className='form-group-max' onSubmit={handleSubmit}>
-      <div className='form-input-container-username'>
-      <div className='form-input-container-group'>
-        <label className='form-label-text'>Username</label>
-        <input className='input-container-username' type="text" name="username" onChange={handleInputChange} />
-      </div>
-      <div className='form-input-container-group'>
-        <label className='form-label-text'>Email Address</label>
-        <input className='input-container-email' type="email" name="email" onChange={handleInputChange} />
-      </div>
-      </div>
-      <div className='form-input-container-group'>
-        <label className='form-label-text'>Password</label>
-        <input className='input-container-password' type="password" name="password1" onChange={handleInputChange} />
-      </div>
-      <div className='form-input-container-group'>
-        <label className='form-label-text'>Confirm Password</label>
-        <input className='input-container-password' type="password" name="password2" onChange={handleInputChange} />
-      </div>
-      <button className='form-input-submit' type="submit">Create Account</button>
-    </form>
-    </div>
-    )}
+      )}
     </div>
   );
 };
