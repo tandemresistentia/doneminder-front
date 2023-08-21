33f1f41ee4d89122ab73e3ae5173b5d134ec15ec
Luis <deathghoul41@gmail.com>
2023-08-22 00:17:06 +0200

    final

--- src/components/Auth/Signup/RegistrationForm.jsx
+++ src/components/Auth/Signup/RegistrationForm.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Login from '../../Auth/Login/LoginForm'
-import './RegistrationForm.css'
+import Login from '../../Auth/Login/LoginForm';
+import './RegistrationForm.css';
 import { GoogleLogin } from 'react-google-login';
 import BASE_URL from '../../config';
-import Google from '../../../assets/Login/google.png'
+import Google from '../../../assets/Login/google.png';
 
 import GoogleAuth from '../GoogleAuth';
 const RegistrationForm = () => {
+  /* ------------------------Handle Registration ----------------------*/
   const [formData, setFormData] = useState({
     username: '',
     email: '',
@@ -16,10 +17,8 @@ const RegistrationForm = () => {
   });
 
   const [errors, setErrors] = useState({
-    username: '',
     email: '',
-    password1: '',
-    password2: '',
+    passwordMismatch: '',
   });
 
   const handleInputChange = (e) => {
@@ -30,59 +29,80 @@ const RegistrationForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    // Check if passwords match
+    if (formData.password1 !== formData.password2) {
+      setErrors({ ...errors, passwordMismatch: "Passwords do not match" });
+      return;
+    } else {
+      setErrors({ ...errors, passwordMismatch: '' });
+    }
+
+    // Check if email has @ symbol
+    if (!formData.email.includes('@')) {
+      setErrors({ ...errors, email: "Email must contain @ symbol" });
+      return;
+    } else {
+      setErrors({ ...errors, email: '' });
+    }
+
     try {
       const response = await axios.post(`${BASE_URL}/dj-rest-auth/registration/`, formData);
       console.log('Registration successful', response.data);
-      window.location.reload()
+      window.location.reload();
     } catch (error) {
       console.error('Registration failed', error);
-
-      // Update errors state with validation errors from the server
-      if (error.response && error.response.data) {
-        setErrors(error.response.data);
-      }
     }
   };
 
+  /* ------------------------Handle Registration ----------------------*/
   const [showLogin, setShowLogin] = useState(false);
 
   const handleShowLogin = () => {
     setShowLogin(!showLogin);
-  }
+  };
+
+  /* ------------------------Toggle Login ---------------------- */
 
   return (
     <div className='app-login-group'>
-      {showLogin ? (<Login/>) : (
+      {showLogin ? (<Login />) : (
         <div className='app-login'>
           <div className='app-registration-leftside'>
             <div className='app-registration-title-group'>
               <p className='app-registration-title'>Sign up to DoneMinder</p>
-              <button className='app-registration-google'><img src={Google}/> Sign up with Google</button>
+              <button className='app-registration-google'><img src={Google} /> Sign up with Google</button>
             </div>
             <p className='app-registration-already'>Already Registered?
-              <button className='app-registration-already-button' onClick={handleShowLogin}>Sign in</button>
-            </p>
+              <button className='app-registration-already-button' onClick={handleShowLogin}>Sign in</button></p>
           </div>
           <div className='app-registration-middle-line'>
-            <div className='line-1'/>
+            <div className='line-1' />
             <p className='middle-text-or'>Or</p>
-            <div className='line-1'/>
+            <div className='line-1' />
           </div>
           <form className='form-group-max' onSubmit={handleSubmit}>
             <div className='form-input-container-username'>
               <div className='form-input-container-group'>
                 <label className='form-label-text'>Username</label>
-                <input
-                  className='input-container-username'
-                  type="text"
-                  name="username"
-                  onChange={handleInputChange}
-                />
-                <span className='error-message'>{errors.username}</span>
+                <input className='input-container-username' type="text" name="username" onChange={handleInputChange} />
               </div>
-              {/* Other input fields */}
-              <button className='form-input-submit' type="submit">Create Account</button>
+              <div className='form-input-container-group'>
+                <label className='form-label-text'>Email Address</label>
+                <input className='input-container-email' type="email" name="email" onChange={handleInputChange} />
+                <span className='error-message'>{errors.email}</span>
+              </div>
             </div>
+            <div className='form-input-container-group'>
+              <label className='form-label-text'>Password</label>
+              <input className='input-container-password' type="password" name="password1" onChange={handleInputChange} />
+            </div>
+            <div className='form-input-container-group'>
+              <label className='form-label-text'>Confirm Password</label>
+              <input className='input-container-password' type="password" name="password2" onChange={handleInputChange} />
+              <span className='error-message'>{errors.passwordMismatch}</span>
+            </div>
+            <button className='form-input-submit' type="submit">Create Account</button>
           </form>
         </div>
       )}
